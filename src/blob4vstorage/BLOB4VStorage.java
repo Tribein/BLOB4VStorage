14058b206be38618b9c2a05469bc4bb6b06a936c
Tribein <tribein@live.ru>
2019-09-09 18:37:24 +0300

    Update BLOB4VStorage.java

--- src/blob4vstorage/BLOB4VStorage.java
+++ src/blob4vstorage/BLOB4VStorage.java
@@ -4,8 +4,11 @@ import java.lang.management.ManagementFactory;
 import java.sql.Connection;
 import javax.servlet.annotation.WebServlet;
 import org.eclipse.jetty.jmx.MBeanContainer;
+import org.eclipse.jetty.server.Connector;
 import org.eclipse.jetty.server.Server;
+import org.eclipse.jetty.server.ServerConnector;
 import org.eclipse.jetty.servlet.ServletHandler;
+import org.eclipse.jetty.util.thread.QueuedThreadPool;
 
 @WebServlet(name="BLOB4VStorage", urlPatterns={"/"})
 public class BLOB4VStorage
@@ -26,12 +29,25 @@ public class BLOB4VStorage
     
     System.setProperty("org.eclipse.jetty.util.log.class", "org.eclipse.jetty.util.log.StdErrLog");
     System.setProperty("org.eclipse.jetty.LEVEL", "OFF");
+    int maxThreads = 8192;
+    int minThreads = 64;
+    int idleTimeout = 100;
+ 
+    QueuedThreadPool threadPool = new QueuedThreadPool(maxThreads, minThreads, idleTimeout);
+    threadPool.setDetailedDump(false);
+    threadPool.setMinThreads(16);
+    threadPool.setMaxThreads(1024);
     
-    Server server = new Server(port);
+    
+    Server server = new Server(threadPool);
+    ServerConnector connector = new ServerConnector(server);
+    connector.setPort(port);
+    connector.setAcceptQueueSize(128);
+    server.setConnectors(new Connector[] {connector});       
     
     MBeanContainer mbContainer = new MBeanContainer(ManagementFactory.getPlatformMBeanServer());
     server.addBean(mbContainer);
-    
+        
     ServletHandler handler = new ServletHandler();
     server.setHandler(handler);
     handler.addServletWithMapping(RequestProcessor.class, "/*");
